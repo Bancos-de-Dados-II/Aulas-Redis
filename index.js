550d53a0b1292368a27dd5b1029c0af60744df5e
Paulo Freitas <paulo.freitas.nt@gmail.com>
2022-11-23 14:10:06 -0300

    Criando as funções de salvar e buscar

---
+++ index.js
@@ -0,0 +1,28 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+async function connectar(){
+    client.on('error', (err) => console.log('Redis Client Error', err));
+    await client.connect();
+}
+
+async function salvar(obj){
+    const saida = await client.set(obj.email, JSON.stringify(obj));
+    console.log(saida);
+}
+
+async function buscar(email){
+    const saida = await client.get(email);
+    console.log(JSON.parse(saida));
+}
+
+const objeto = {
+    nome: 'paulo',
+    email: 'paulo@gmail.com'
+}
+
+connectar();
+// salvar(objeto);
+buscar('paulo@gmail.com');
+
